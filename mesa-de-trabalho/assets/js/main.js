1771526e5794012a4b22419f7745f4d367921b80
gutoffetti <gu_toffetti@hotmail.com>
2022-02-23 20:38:09 -0300

    Jogada 3 feita por Gustavo Toffetti

--- mesa-de-trabalho/assets/js/main.js
+++ mesa-de-trabalho/assets/js/main.js
@@ -5,3 +5,7 @@ l2c2.innerText = "X";
 var jogada = document.querySelectorAll(".coordenada");
 
 jogada[2].innerText = "O";
+
+var l3c3 = document.getElementById("linha-3_coluna-1");
+
+l3c3.innerText = "X";
